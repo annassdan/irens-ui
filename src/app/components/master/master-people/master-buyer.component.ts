dc8aa002a92a74c3e2da2e71bd11839a53509de7
iPUL <ipul640@gmail.com>
2019-07-10 18:17:00 +0700

    add input customer and item

--- src/app/components/master/master-people/master-buyer.component.ts
+++ src/app/components/master/master-people/master-buyer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {CustomerErrorStateMatchers, CustomerForm} from '../../../inits/Customer-init';
+import {ItemService} from '../../../services/item.service';
+import {MatSnackBar} from '@angular/material';
+import {PeopleService} from '../../../services/people.service';
+import {errorSnackBar, successSnackBar} from '../../../shared/constants';
 
 @Component({
   selector: 'app-master-buyer',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterBuyerComponent implements OnInit {
 
-  constructor() { }
+
+  page = 0;
+  size = 10;
+
+
+  matchers = CustomerErrorStateMatchers;
+  formUtama = CustomerForm();
+  constructor(private masterCustomer: PeopleService,
+              public snackBar: MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  post(a) {
+    this.masterCustomer.post(a).subscribe(
+      (status) => {
+        // successSnackBar('this.snack');
+        this.get();
+      },
+      (e) => errorSnackBar(this.snackBar, 'GAGAL'),
+    );
+  }
+
+  get() {
+    this.masterCustomer.getAll(this.page, this.size).subscribe(
+      (v: any[]) => {
+        console.log(v)
+      },
+      (error1) => errorSnackBar(this.snackBar, 'GAGAL'),
+      () => {
+      }
+    );
+  }
+
+  delete(id) {
+    return this.masterCustomer.delete(id).subscribe(
+      (status) => {
+        successSnackBar('this.snack');
+        this.get();
+      },
+      (e) => errorSnackBar(this.snackBar, 'GAGAL'),
+    );
   }
 
 }
